21df4a6d0d63ef984648984e1139278a084ec5da
mapogolions <ivanovpavelalex45@gmail.com>
2019-12-28 14:59:02 +0500

    Laid the foundation for future tests

---
+++ tests/reducer.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const test = require('ava');
+const { arrayOf } = require('../reducer.js');
+
+test('arrayOf reducer', t => {
+  const reducer = arrayOf();
+  t.deepEqual(reducer.init, []);
+  t.deepEqual(reducer.step([], 1), [1]);
+  t.is(reducer.done(1), 1);
+});
