0d242f4754a9567cdbb95d7ea43507b57550150c
mapogolions <ivanovpavelalex45@gmail.com>
2019-12-30 00:41:08 +0500

    Migrate to shape function

--- src/reducers.js
+++ src/reducers.js
@@ -1,46 +1,52 @@
 'use strict';
 
-const { identity } = require('./tools.js');
 
+function zeroArity(args) {
+  return args && (args.length === 0);
+}
+
+function oneArity(args) {
+  return args.length === 1;
+}
+
+function shapeFn(initial, fn) {
+  return function (...varargs) {
+    if (zeroArity(varargs)) return initial;
+    if (oneArity(varargs)) return varargs[0];
+    return fn(...varargs);
+  };
+}
 
 function arrayOf() {
-  return {
-    init: () => [],
-    step: (acc, x) => { acc.push(x); return acc; },
-    done: identity
-  };
+  return shapeFn([], (acc, x) => {
+    acc.push(x);
+    return acc;
+  });
 }
 
 function setOf() {
-  return {
-    init: () => new Set(),
-    step: (acc, x) => { acc.add(x); return acc; },
-    done: identity
-  };
+  return shapeFn(new Set(), (acc, x) => {
+    acc.add(x);
+    return acc;
+  });
 }
 
 function mapOf() {
-  return {
-    init: () => new Map(),
-    step: (acc, [key, value]) => { acc.set(key, value); return acc; },
-    done: identity
-  };
+  return shapeFn(new Map(), (acc, [key, value]) => {
+    acc.set(key, value);
+    return acc;
+  });
 }
 
 function assoc() {
-  return {
-    init: () => ({}),
-    step: (acc, [key, value]) => { acc[key] = value; return acc; },
-    done: identity
-  };
+  return shapeFn({}, (acc, [key, value]) => {
+    acc[key] = value;
+    return acc;
+  });
 }
 
 function stringOf(sep = '') {
-  return {
-    init: () => '',
-    step: (acc, x) => `${acc}${sep}${x}`,
-    done: identity
-  };
+  return shapeFn('', (acc, x) => `${acc}${sep}${x}`);
 }
 
 
