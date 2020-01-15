9ff853f6c3c9c2b4cdaf79289f8c5231d94bb97a
mapogolions <ivanovpavelalex45@gmail.com>
2020-01-15 22:16:02 +0500

    Remove 'reducer' prefix

--- tests/primitives.test.js
+++ tests/primitives.test.js
@@ -9,31 +9,31 @@ const {
   stringOf
 } = require('../src/primitives.js')
 
-test('stringOf reducer', t => {
+test('stringOf primitive', t => {
   t.is(stringOf(), '')
   t.is(stringOf('', '1'), '1')
   t.is(stringOf(1), 1)
 })
 
-test('assoc reducer', t => {
+test('assoc primitive', t => {
   t.deepEqual(assoc(), {})
   t.deepEqual(assoc({}, [1, 1]), { 1: 1 })
   t.is(assoc(1), 1)
 })
 
-test('setOf reducer', t => {
+test('setOf primitive', t => {
   t.deepEqual(setOf(), new Set())
   t.deepEqual(setOf(new Set(), 1), new Set([1]))
   t.is(setOf(1), 1)
 })
 
-test('mapOf reducer', t => {
+test('mapOf primitive', t => {
   t.deepEqual(mapOf(), new Map())
   t.deepEqual(mapOf(new Map(), ['one', 1]), new Map([['one', 1]]))
   t.is(mapOf(1), 1)
 })
 
-test('arrayOf reducer', t => {
+test('arrayOf primitive', t => {
   t.deepEqual(arrayOf(), [])
   t.deepEqual(arrayOf([], 1), [1])
   t.is(arrayOf(1), 1)
